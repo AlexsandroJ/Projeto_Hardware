4e1c50cff95beac403e0cbea7e659f6ee1097062
Zenio99 <39632694+Zenio99@users.noreply.github.com>
2019-04-28 13:21:33 -0300

    Sign Extend corrigido

--- Seinal_Extend.sv
+++ Seinal_Extend.sv
@@ -6,19 +6,7 @@ module Sinal_Extend(
     always_comb begin
 
         if(Sinal_In[6:0]==7'd3) begin//tipo I
-            if(Sinal_In[14:12]==3'd4 || Sinal_In[14:12]==3'd5 || Sinal_In[14:12]==3'd6) begin //lbu, lhu, lwu(extensão unsigned)
-                Sinal_Out = { 52'd0, Sinal_In[31:20] };
-            end
-            else begin
-                if(Sinal_In[14:12]==3'd0 || Sinal_In[14:12]==3'd2) begin //ld, lb, lh, lw(extensão signed)
-                    if(Sinal_In[31]==0) begin
-                        Sinal_Out = { 52'd0, Sinal_In[31:20] };
-                    end
-                    else begin
-                        Sinal_Out = { 52'd1, Sinal_In[31:20] };
-                    end    
-                end
-            end     
+            Sinal_Out = { 52'd0, Sinal_In[31:20] };
         end
         else begin
             if(Sinal_In[6:0]==7'd19) begin
@@ -33,14 +21,37 @@ module Sinal_Extend(
             end
             else begin
                 if(Sinal_In[6:0]==7'd35) begin //tipo S(extensão signed)
-                    if(Sinal_In[31]==0) begin
-                        Sinal_Out = { 52'd0, Sinal_In[31:25], Sinal_In[11:7] };
+                    Sinal_Out = { 52'd0, Sinal_In[31:25], Sinal_In[11:7] };                      
+                end
+                else begin
+                    if(Sinal_In[6:0]==7'd99) begin //tipo SB
+                        Sinal_Out = { 51'd0, Sinal_In[31], Sinal_In[7], Sinal_In[30:25], Sinal_In[11:8], 1'd0 };
                     end
                     else begin
-                        if(Sinal_In[31]==1) begin
-                            Sinal_Out = { 52'd1, Sinal_In[31:25], Sinal_In[11:7] };
+                        if(Sinal_In[6:0]==7'd103 && Sinal_In[14:12]!=3'd0) begin //tipo SB
+                            Sinal_Out = { 51'd0, Sinal_In[31], Sinal_In[7], Sinal_In[30:25], Sinal_In[11:8], 1'd0 };
                         end
-                    end    
+                        else begin
+                            if(Sinal_In[6:0]==7'd55) begin //tipo U
+                                if(Sinal_In[31]==0) begin
+                                    Sinal_Out = { 32'd0, Sinal_In[31:12], 12'd0 };
+                                end
+                                else begin
+                                    Sinal_Out = { 32'd1, Sinal_In[31:12], 12'd0 };
+                                end
+                            end
+                            else begin
+                                if(Sinal_In[6:0]==7'd111) begin //tipo UJ
+                                    if(Sinal_Out[31]==0) begin
+                                        Sinal_Out = { 43'd0, Sinal_In[31], Sinal_In[19:12], Sinal_In[20], Sinal_In[30:21], 1'd0 };
+                                    end
+                                    else begin
+                                        Sinal_Out = { 43'd1, Sinal_In[31], Sinal_In[19:12], Sinal_In[20], Sinal_In[30:21], 1'd0 };
+                                    end
+                                end
+                            end
+                        end
+                    end
                 end
             end            
         end
