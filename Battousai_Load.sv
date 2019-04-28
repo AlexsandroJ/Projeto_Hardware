4044bca14492f65ada499d08f8046e64871479ee
Zenio99 <39632694+Zenio99@users.noreply.github.com>
2019-04-28 12:05:27 -0300

    Battousai corrigido

--- Battousai_Load.sv
+++ Battousai_Load.sv
@@ -7,22 +7,53 @@ module Battousai_Load (
     always_comb begin
         case(Register_Intruction_Instr31_0[6:0])
             7'd3: begin
-                if(Register_Intruction_Instr31_0[14:12]==3'd0 || Register_Intruction_Instr31_0[14:12]==3'd4) begin //lb rd, imm(rs1) || lbu rd, imm(rs1)
+                if(Register_Intruction_Instr31_0[14:12]==3'd4) begin //lbu rd, imm(rs1)
                     Saida_Memory_Data = { 56'd0, Dataout[7:0] };
                 end
                 else begin
-                    if(Register_Intruction_Instr31_0[14:12]==3'd1 || Register_Intruction_Instr31_0[14:12]==3'd5) begin //lh rd, imm(rs1) || lhu rd, imm(rs1)
+                    if(Register_Intruction_Instr31_0[14:12]==3'd5) begin //lhu rd, imm(rs1)
                         Saida_Memory_Data = { 48'd0, Dataout[15:0] };
                     end
                     else begin
-                        if(Register_Intruction_Instr31_0[14:12]==3'd2 || Register_Intruction_Instr31_0[14:12]==3'd6) begin //lw rd, imm(rs1) || lwu rd, imm(rs1)
+                        if(Register_Intruction_Instr31_0[14:12]==3'd6) begin //lwu rd, imm(rs1)
                             Saida_Memory_Data = { 32'd0, Dataout[31:0] };
+                        end
+                        else begin
+                            if(Register_Intruction_Instr31_0[14:12]==3'd0) begin //lb rd, imm(rs1)
+                                if(Dataout[7]==1'd0) begin
+                                    Saida_Memory_Data = { 56'd0, Dataout[7:0] };
+                                end
+                                else begin
+                                    Saida_Memory_Data = { 56'd1, Dataout[7:0] };
+                                end    
+                            end
+                            else begin
+                                if(Register_Intruction_Instr31_0[14:12]==3'd1) begin //lh rd, imm(rs1)
+                                    if(Dataout[15]==1'd0) begin
+                                        Saida_Memory_Data = { 48'd0, Dataout[15:0] };
+                                    end
+                                    else begin
+                                        Saida_Memory_Data = { 48'd1, Dataout[15:0] };
+                                    end    
+                                end
+                                else begin
+                                    if(Register_Intruction_Instr31_0[14:12]==3'd2) begin //lw rd, imm(rs1)
+                                        if(Dataout[31]==1'd0) begin
+                                            Saida_Memory_Data = { 32'd0, Dataout[31:0] };
+                                        end
+                                        else begin
+                                            Saida_Memory_Data = { 32'd1, Dataout[31:0] };
+                                        end    
+                                    end
+                                    else begin //ld rd, imm(rs1)
+                                        Saida_Memory_Data = Dataout;   
+                                    end
+                                end
+                            end
                         end
                     end
                 end
-            end    
-            default:
-                Saida_Memory_Data = Dataout;
+            end
         endcase
     end
 
--- Battousai_Store.sv
+++ Battousai_Store.sv
@@ -8,15 +8,33 @@ module Battousai_Store (
         case(Register_Intruction_Instr31_0[6:0])
             7'd35: begin
                 if(Register_Intruction_Instr31_0[14:12]==3'd0) begin //sb rd, imm(rs1)
-                    DadoIn_64 = { 56'd0, Reg_B_Out[7:0] };
+                    if(Reg_B_Out[7]==0) begin
+                        DadoIn_64 = { 56'd0, Reg_B_Out[7:0] };
+                    end
+                    else begin
+                        DadoIn_64 = { 56'd1, Reg_B_Out[7:0] };
+                    end    
                 end
                 else begin
                     if(Register_Intruction_Instr31_0[14:12]==3'd1) begin //sh rd, imm(rs1)
-                        DadoIn_64 = { 48'd0, Reg_B_Out[15:0] };
+                        if(Reg_B_Out[15]==0) begin
+                            DadoIn_64 = { 48'd0, Reg_B_Out[15:0] };
+                        end
+                        else begin
+                            DadoIn_64 = { 48'd1, Reg_B_Out[15:0] };
+                        end    
                     end
                     else begin
                         if(Register_Intruction_Instr31_0[14:12]==3'd2) begin //sw rd, imm(rs1)
-                            DadoIn_64 = { 32'd0, Reg_B_Out[31:0] };
+                            if(Reg_B_Out[31]==0) begin
+                                DadoIn_64 = { 32'd0, Reg_B_Out[31:0] };
+                            end
+                            else begin
+                                DadoIn_64 = { 32'd1, Reg_B_Out[31:0] };
+                            end
+                        end
+                        else begin //sd rd, imm(rs1)
+                            DadoIn_64 = Reg_B_Out;
                         end
                     end
                 end
